a3b77cf4bbbfda9e915d2612ac9406fa4b96a8f9
Paola Sbert <paosbert@gmail.com>
2023-02-16 11:52:38 -0300

    Creando las rutas de TODO Machine

---
+++ src/routes/home/HomePage.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { useTodos } from '../useTodos';
+import { TodoHeader } from '../../UI/TodoHeader';
+import { TodoCounter } from '../../UI/TodoCounter';
+import { TodoSearch } from '../../UI/TodoSearch';
+import { TodoList } from '../../UI/TodoList';
+import { TodoItem } from '../../UI/TodoItem';
+import { TodosError } from '../../UI/TodosError';
+import { TodosLoading } from '../../UI/TodosLoading';
+import { EmptyTodos } from '../../UI/EmptyTodos';
+import { TodoForm } from '../../UI/TodoForm';
+import { CreateTodoButton } from '../../UI/CreateTodoButton';
+import { Modal } from '../../UI/Modal';
+import { ChangeAlert } from '../../UI/ChangeAlert';
+
+function HomePage() {
+  const { state, stateUpdaters } = useTodos();
+
+  const {
+    error,
+    loading,
+    searchedTodos,
+    totalTodos,
+    completedTodos,
+    openModal,
+    searchValue,
+  } = state;
+
+  const {
+    setOpenModal,
+    addTodo,
+    completeTodo,
+    deleteTodo,
+    setSearchValue,
+    sincronizeTodos,
+  } = stateUpdaters;
+  
+  return (
+    <React.Fragment>
+      <TodoHeader loading={loading}>
+        <TodoCounter
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </TodoHeader>
+
+      <TodoList
+        error={error}
+        loading={loading}
+        totalTodos={totalTodos}
+        searchedTodos={searchedTodos}
+        searchText={searchValue}
+        onError={() => <TodosError />}
+        onLoading={() => <TodosLoading />}
+        onEmptyTodos={() => <EmptyTodos />}
+        onEmptySearchResults={
+          (searchText) => <p>No hay resultados para {searchText}</p>
+        }
+      >
+        {todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        )}
+      </TodoList>
+
+      {!!openModal && (
+        <Modal>
+          <TodoForm
+            addTodo={addTodo}
+            setOpenModal={setOpenModal}
+          />
+        </Modal>
+      )}
+
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+
+      <ChangeAlert
+        sincronize={sincronizeTodos}
+      />
+    </React.Fragment>
+  );
+}
+
+export { HomePage };
