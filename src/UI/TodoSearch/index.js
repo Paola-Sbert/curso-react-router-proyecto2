8165ce0feaa137de62cadeae8dd3d42e86adb682
Paola Sbert <paosbert@gmail.com>
2023-02-18 16:57:51 -0300

    Reto: página de búsquedas con navegación

--- src/UI/TodoSearch/index.js
+++ src/UI/TodoSearch/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import './TodoSearch.css';
 
 function TodoSearch({ searchValue, setSearchValue, loading }) {
+  const [ params, setParams ] = useSearchParams();
+  const paramValue = params.get('search');
+
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
     setSearchValue(event.target.value);
+    
+    let param = {
+      search: event.target.value,
+    }
+
+    setParams(param);
   };
+
+  useEffect(() => {
+    const search = params.get('search') ?? '';
+    setSearchValue(search);
+  }, [params])
 
   return (
     <input
       className="TodoSearch"
-      placeholder="Cebolla"
+      placeholder="Busca un ToDo"
       value={searchValue}
       onChange={onSearchValueChange}
       disabled={loading}
